33253c5df785729a8075caa8b28b270ff0f5f03a
Guilherme <guilhermemcl1503@gmail.com>
2025-06-02 20:47:05 -0300

    Inicando Projeto

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { app } from './app'
+import { env } from './env'
+
+
+
+
+app
+    .listen({
+        host: '0.0.0.0',
+        port: env.PORT,
+    })
+    .then(() => {
+        console.log('Servidor rodando em http://localhost:', env.PORT)
+    })
