20172081884aa313a89e3a0f56125db27fdd5d1d
Guilherme <guilhermemcl1503@gmail.com>
2025-06-02 21:27:45 -0300

    Feat: Criando schem de validação com ZOD

---
+++ src/validations/vehicleSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+function documentoValido(doc: string): boolean {
+    return /^\d{11}$/.test(doc) || /^\d{14}$/.test(doc);
+}
+
+export const vehicleSchema = z.object({
+    vin: z.string().min(1),
+    placa: z.string().min(1),
+    module: z.string().min(1),
+    data_entrega: z.coerce.date(),
+    data_fabricacao: z.coerce.date(),
+    data_venda: z.coerce.date(),
+    pais_operacao: z.string().min(1),
+    consessionaria_venda: z.string().min(1),
+    data_ultimo_reparo: z.coerce.date(),
+    documento_proprietario: z.string().refine((doc) => documentoValido(doc), {
+        message: 'Documento inválido'
+    }),
+})
+
+export const parcialVehicleSchema = vehicleSchema.partial()
