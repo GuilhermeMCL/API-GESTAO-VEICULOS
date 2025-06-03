452d41d87743ba95fe7a8d15888a72329de942c7
Guilherme <guilhermemcl1503@gmail.com>
2025-06-02 22:17:54 -0300

    Fix : Registro de Rota

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify";
+import { vehicleRoutes } from "./routes/vehicleRoutes";
 
 
 export const app = fastify();
+app.register(vehicleRoutes, {
+    prefix: '/vehicles'
+});
 
 
